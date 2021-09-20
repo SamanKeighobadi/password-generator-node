dbfdfd5f10c88b4134ca9d7528fa38a46eeb8a2f
saman keighobadi <saman.keighobadi@gmail.com>
2021-09-20 13:01:45 +0430

    added to clipborad

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const chalk = require("chalk");
 const { program } = require("commander");
+const clipboard = require('clipboardy');
 
-const {createPassword} = require('./utils/createPassword');
+const { createPassword } = require("./utils/createPassword");
 
 program.version("1.0.0").description("Simple Node.js Password Generator");
 program
   .option("-l,--length <number>", "Length of password", "9")
-  .option("-s,--save", "Save password to Password.txt")
   .option("-nn,--no-numbers", "No numbers in  password")
   .option("-ns,--no-symbols", "No symbols in  password")
-  .option('-lc,--no-lowercase',"No lower case letter in  password")
-  .option('-uc,--no-uppercase',"No upper case letter in  password")
   .parse();
 
-const {length,save,numbers,symbols,lowercase,uppercase} = program.opts()
+const { length, save, numbers, symbols } = program.opts();
 
+const generatedPassword = createPassword(length, numbers, symbols);
 
+clipboard.writeSync(generatedPassword);
 
-const generatedPassword = createPassword(length,numbers,symbols)
+console.log(chalk.greenBright('Password Generated: '),chalk.redBright(generatedPassword));
+console.log(chalk.cyanBright("Password copied to clipbaord"))
